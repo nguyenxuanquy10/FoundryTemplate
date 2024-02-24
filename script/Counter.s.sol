da08f7f1c6a461b83b27b3f5f5e867944c8000fd
quynxkba1 <quynx@twendeesoft.com>
2024-02-24 20:51:43 +0700

    update: config

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
+    Counter counter; 
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivatekey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivatekey);
+        counter = new Counter();
+        vm.stopBroadcast();
     }
 }
